dd2b7403cab08dc445dc81989f4854d26507062f
standard1995 <takashikondo1995@gmail.com>
2025-09-19 05:22:49 -0700

    feat: goback button

--- src/pages/End.jsx
+++ src/pages/End.jsx
@@ -1,13 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import { MarksContext } from "../App";
 import { useContext } from "react";
 
 const End = () => {
     const [marks, setMarks] = useContext(MarksContext);
+    const navigate = useNavigate();
 
-
+    const goBack = () => {
+        navigate('/');
+    }
     return (
-        <div className="bg-blue-500 h-full w-full flex justify-center items-center">
+        <div className="bg-blue-500 h-full w-full flex flex-col justify-center items-center gap-4">
             <h1 className="text-6xl text-amber-400 font-black">Your Score is {marks}</h1>
+            <button onClick={() => goBack()} className="bg-[#ffe364ab] h-[60px] w-[120px] rounded-xl font-black text-blue-700 text-xl border-double border-4 border-sky-500 hovor: cursor-pointer hover:bg-emerald-300 md:animate-pulse">Go Back</button>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="w-full h-full text-center p-8 flex flex-col justify-center items-center  gap-32" style={{ backgroundImage: `url(${background})` }}>
             <h1 className='text-amber-200 text-7xl font-serif font-black'>Simple Select Quiz Game</h1>
             <h2 className='text-6xl text-blue-100'>How Much Common Sense Do You Know?</h2>
-            <button className='text-7xl hover:cursor-pointer rounded-xl outline-4 animate-bounce text-purple-200 p-10' onClick={start}>Start</button>
+            <button className='text-7xl bg-[#13775f6e] hover:cursor-pointer rounded-xl outline-4 animate-bounce text-purple-500 p-10' onClick={start}>Start</button>
         </div>
     )
 }
